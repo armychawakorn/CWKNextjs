e94f6475d965fc55ea694c97110d5adac45fb8e8
chawakorn <armychawakorn@gmail.com>
2024-09-01 23:52:24 +0700

    .

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,8 +1,8 @@
-"use client"
+'use client'
 import React, { useEffect, useState } from 'react'
 
-export default function page({ params }: { params: { name: string } }) {
-    const [pokemon, setPokemon] = useState<any | null>(null);
+export default function Pokemon({ params }: { params: { name: string } }) {
+    const [Pokemon, setPokemon] = useState<any | null>(null);
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -17,6 +17,10 @@ export default function page({ params }: { params: { name: string } }) {
         fetchData();
     }, [])
     return (
-        <div></div>
+        <div className='grid grid-col-1 justify-center'>
+            <div style={{ border: '1px solid black', padding: '10px', margin: '10px', borderRadius: '5px' }}>
+                <img src={Pokemon?.sprites.front_default} alt={params.name} style={{ height: '256px', width: '256px' }} />
+            </div>
+        </div>
     )
 }
